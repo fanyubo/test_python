fd905826b5ca0474f25d50075bac65c407b738c3
Yubo Fan <fanyubo@gmail.com>
2018-01-07 00:37:55 +0000

    append, del, +, * of list.

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-mylist = [1, 2, 3, 4]
+# basic list
+mylist = [1, 3, 5, 7]
 print(mylist)
 
-newlist = mylist * 2
+# list multiplication
+newlist = mylist * 3
 print(newlist)
 newlist.sort()
 print(newlist)
+
+# append and delete
+mylist.append(9)
+print(mylist)
+print(mylist[2])
+del mylist[2]
+print(mylist)
+
+# length
+print(len(mylist))
+print(mylist)
+
+# adding lists
+stringlist = ['git', 'has', 'good', 'stuff']
+print(mylist + stringlist)
+
+# multiply list
+print(stringlist * 3)
